59d19ccb3beb2e89da23841ab51ea2fd8241593d
parkjin21004 <parkjin21004@gmai.com>
2025-06-13 15:10:21 +0900

    Update: base url '/api'로 변경

--- src/main/java/com/skkuswtich/leaderboard/leaderboard/controller/TeamController.java
+++ src/main/java/com/skkuswtich/leaderboard/leaderboard/controller/TeamController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/team")
+@RequestMapping("/api/team")
 @RequiredArgsConstructor
 public class TeamController {
     private final LeaderboardService leaderboardService;
